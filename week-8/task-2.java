b18f91b93124486cda213fb98765df1db57446d4
somaiya <somaiyak4358@gmail.com>
2026-09-21 11:11:26 +0530

    Add FRUIT class with details and display methods

---
+++ week-8/task-2.java
@@ -0,0 +1,33 @@
+public class FRUIT {
+    
+    // Data members
+    String color;
+    String taste;
+    double price;
+
+    // Method to set values
+    void setDetails(String c, String t, double p) {
+        color = c;
+        taste = t;
+        price = p;
+    }
+
+    // Method to display values
+    void display() {
+        System.out.println("Color: " + color);
+        System.out.println("Taste: " + taste);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        
+        // Creating object
+        FRUIT f = new FRUIT();
+
+        // Setting values
+        f.setDetails("Red", "Sweet", 80);
+
+        // Displaying values
+        f.display();
+    }
+}
