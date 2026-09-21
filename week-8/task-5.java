7339cd38d75588a2c53633bb2b59593abd5d0e45
somaiya <somaiyak4358@gmail.com>
2026-09-21 12:25:30 +0530

    Refactor FRUIT class into static nested class

--- week-8/task-5.java
+++ week-8/task-5.java
@@ -1,38 +1,41 @@
-public class FRUIT {
+public class Task5 {
 
-    // Data members
-    String color;
-    String taste;
-    double price;
+    static class FRUIT {
 
-    // Default constructor
-    FRUIT() {
-        this("Unknown", "Unknown", 0);
-    }
+        // Data members
+        String color;
+        String taste;
+        double price;
 
-    // One-argument constructor
-    FRUIT(String c) {
-        this(c, "Unknown", 0);
-    }
+        // Default constructor
+        FRUIT() {
+            this("Unknown", "Unknown", 0);
+        }
 
-    // Two-argument constructor
-    FRUIT(String c, String t) {
-        this(c, t, 0);
-    }
+        // One-argument constructor
+        FRUIT(String c) {
+            this(c, "Unknown", 0);
+        }
 
-    // Three-argument constructor
-    FRUIT(String c, String t, double p) {
-        color = c;
-        taste = t;
-        price = p;
-    }
+        // Two-argument constructor
+        FRUIT(String c, String t) {
+            this(c, t, 0);
+        }
 
-    // Display method
-    void display() {
-        System.out.println("Color: " + color);
-        System.out.println("Taste: " + taste);
-        System.out.println("Price: " + price);
-        System.out.println();
+        // Three-argument constructor
+        FRUIT(String c, String t, double p) {
+            color = c;
+            taste = t;
+            price = p;
+        }
+
+        // Display method
+        void display() {
+            System.out.println("Color: " + color);
+            System.out.println("Taste: " + taste);
+            System.out.println("Price: " + price);
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
